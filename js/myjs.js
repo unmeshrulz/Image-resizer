bd0b7ad6e5bf1c7c1329a8767a1f5f8bfe6886a5
Unmesh <unmesh@dronamobile.com>
2018-08-23 19:33:57 +0530

    1. renamed res.json to sizes.json 2. added support for android image creation also 3. (ToDo) Just need to modify sizes.json for android diffrent image sizes

--- js/myjs.js
+++ js/myjs.js
@@ -1,7 +1,6 @@
 let fs = require('fs');
 let Q = require('q');
 let ifFileExists = 0;
-let testArray = [];
 
 let readURL = (input) => {
     if (input.files && input.files[0]) {
@@ -33,53 +32,75 @@ let createResizedImage = (ogFilePath, fileName, fileExtention, imgWidth, imgHeig
     return deferred.promise;
 }
 
+let loopToResize = (ogFilePath, fileName, getDataToResizeContent, fileExtention) => {
+    for (let i = 0; i < getDataToResizeContent.length; i++) {
+        let setParams = getDataToResizeContent[i];
+        createResizedImage(ogFilePath, fileName + setParams.name, fileExtention, setParams.width, setParams.height).then(function () {
+            console.log("Image Saved");
+        });
+    }
+}
+
 let letsResize = () => {
     $('#resizeIt').click(function(e){
         e.preventDefault();
-        let imgHeight = imgWidth = 0;
-        imgHeight = parseInt($('#imgHeight').val());
-        imgWidth = parseInt($('#imgWidth').val());
-        let checkBox = $('.form-check-input')[0].checked;
-        if (ifFileExists && checkBox == 1) {
+        let imgHeight       = imgWidth = 0;
+        imgHeight           = parseInt($('#imgHeight').val());
+        imgWidth            = parseInt($('#imgWidth').val());
+        let checkBox        = $('.form-check-input')[0].checked;
+        let androidNoti     = $('.form-check-input')[1].checked;
+        let androidAppIcon  = $('.form-check-input')[2].checked;
+        let custom          = $('.form-check-input')[3].checked;
+         if (ifFileExists) {
             let { dialog } = require('electron').remote;
             dialog.showSaveDialog((fileName) => {
                 if (fileName === undefined) {
                     console.log("You didn't save the file");
                     return;
                 }
-                else {
-                    if (!fs.existsSync(fileName)) {
-                        fs.mkdirSync(fileName);
-                    }
-                    let ogFilePath = ifFileExists.path;
-                    let fileExtention = ogFilePath.split('.').pop();
-                    let jsonData = fs.readFileSync('res.json');
-                    jsonData = JSON.parse(jsonData);
-                    for (let i = 0; i < jsonData.IOS.length; i++) {
-                        let setParams = jsonData.IOS[i];
-                        createResizedImage(ogFilePath, fileName + setParams.name, fileExtention,setParams.width, setParams.height).then(function () {
+                let radioVal = $('input[name=optradio]:checked').val();
+                if (!fs.existsSync(fileName)) {
+                    fs.mkdirSync(fileName);
+                }
+                let ogFilePath = ifFileExists.path;
+                let fileExtention = ogFilePath.split('.').pop();
+                let getDataToResize;
+                switch(radioVal){
+                    case "IOS":
+                        getDataToResize = JSON.parse(fs.readFileSync(__dirname + "/sizes.json")).IOS;
+                        loopToResize(ogFilePath, fileName, getDataToResize, fileExtention);
+                        createResizedImage(ogFilePath, fileName + '/itunesConnect', 'jpg', 1024, 1024).then(function () {
                             console.log("Image Saved");
                         });
+                        break;
+                    case "ANI":
+                        getDataToResize = JSON.parse(fs.readFileSync(__dirname + "/sizes.json")).ANI;
+                        loopToResize(ogFilePath, fileName, getDataToResize, fileExtention);
+                        break;
+                    case "AAI":
+                        getDataToResize = JSON.parse(fs.readFileSync(__dirname + "/sizes.json")).AAI;
+                        loopToResize(ogFilePath, fileName, getDataToResize, fileExtention);
+                        break;
+                    case "CUS":
+                    if (imgHeight > 0 && imgWidth > 0) 
+                        createResizedImage(ogFilePath, fileName, fileExtention, imgWidth, imgHeight).then(function () {
+                            console.log("Successfully Saved The Image");
+                        });
+                    else 
+                        alert("Height Width entered are not valid.");
+                    break;
+                }
+
+
+                    
+                    if (checkBox == 1) {                       
                     }
-                    createResizedImage(ogFilePath, fileName + '/itunesConnect','jpg',1024,1024).then(function () {
-                        console.log("Image Saved");
-                    });
-                }
-            });
-        }
-        else if (ifFileExists && imgHeight > 0 && imgWidth > 0) {
-            let { dialog } = require('electron').remote;
-            dialog.showSaveDialog((fileName) => {
-                if (fileName === undefined) {
-                    console.log("You didn't save the file");
-                    return;
-                }
-                else {
+                
+                // else
+                else if(imgHeight > 0 && imgWidth > 0){
                     let ogFilePath = ifFileExists.path;
                     let fileExtention = ogFilePath.split('.').pop();
-                    createResizedImage(ogFilePath, fileName, fileExtention, imgWidth, imgHeight).then(function () {
-                        alert("Successfully Saved The Image");
-                    });
+
                 }
             });
         }
@@ -94,13 +115,15 @@ let rmImgSelNew = () => {
     });
 }
 
-$('input[type=checkbox]').click(function () {
-    if ($('.form-check-input')[0].checked)
+$('input[type=radio]').click(function () {
+    let chkInput = $('.form-check-input');
+    if (chkInput[0].checked || chkInput[1].checked || chkInput[2].checked)
         $('input[type=number]').prop('disabled', true);
-    else
+    else if(chkInput[3].checked)
         $('input[type=number]').prop('disabled', false);
-}); 
+});
 
 $(document).ready(function () {
     rmImgSelNew();
+    $('.form-check-input')[3].checked = true;
 });
